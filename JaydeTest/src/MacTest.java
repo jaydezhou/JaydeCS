7638ebf96d92bf77ceb0ce7dae9c93d36360dc4b
jaydezhou <jaydezhou@gmail.com>
2017-10-11 19:00:58 +0800

    db

--- JaydeTest/src/MacTest.java
+++ JaydeTest/src/MacTest.java
@@ -1,8 +1,11 @@
+import ensemble.EnsembleApp;
+
 /**
  * Created by zhouyongjian on 2017/10/11.
  */
 public class MacTest {
     public static void main(String[] args) {
         System.out.println("MacTest");
+        EnsembleApp app = new EnsembleApp();
     }
 }
