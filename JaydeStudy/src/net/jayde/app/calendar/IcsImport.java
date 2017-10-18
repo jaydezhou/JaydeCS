cc0a05ff09c8392644bbb534da0ef6fb59777e21
Rainy Dudu <1072874017@qq.com>
2017-10-19 01:26:25 +0800

    my songs

--- JaydeStudy/src/net/jayde/app/calendar/IcsImport.java
+++ JaydeStudy/src/net/jayde/app/calendar/IcsImport.java
@@ -2,14 +2,15 @@ package net.jayde.app.calendar;
 
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
-import net.fortuna.ical4j.model.Calendar;
+import net.fortuna.ical4j.model.*;
+import net.fortuna.ical4j.model.component.CalendarComponent;
+import net.fortuna.ical4j.model.component.VEvent;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class IcsImport {
-    public static String filename = "C:\\Users\\zhouyj\\iCloudDrive\\文档集\\3.技术文档集\\3.1）个人项目\\日历MAC.ics";
+    public static String filename = "C:\\Program Files (x86)\\ideaIU\\IdeaProjects\\日历MAC.ics";
 
     public static void main(String[] args) throws IOException, ParserException {
         FileInputStream fin = new FileInputStream(filename);
@@ -17,5 +18,22 @@ public class IcsImport {
         CalendarBuilder builder = new CalendarBuilder();
 
         Calendar calendar = builder.build(fin);
+//        calendar.validate();
+        ComponentList<CalendarComponent> cl = calendar.getComponents(Component.VEVENT);
+        for (CalendarComponent cc : cl) {
+            System.out.println("------------------------" + cc.getName() + "--------------------------------");
+            System.out.println(cc.getProperties());
+            PropertyList<Property> pl = cc.getProperties();
+//            for (Property p:pl) {
+//                System.out.println("【"+p.getName()+"】"+p.getValue());
+//            }
+//            if(cc.getName().equals(Component.VEVENT)){
+//                VEvent ce = (VEvent) cc;
+//                System.out.println(cc);
+//                System.out.println("\n\n");
+//                System.out.println(ce);
+//                System.out.println(ce.getSummary());
+//
+        }
     }
 }
