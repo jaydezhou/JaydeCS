c940063374aa476e76e5ed750e6a3a5e1715f80d
jaydezhou <jayde@sohu.com>
2018-01-08 11:08:15 +0800

    fit mybatis

---
+++ JaydeStudy/src/net/jayde/study/Algorithms4/c1_3_BagQueryStack/E1_3_1.java
@@ -0,0 +1,10 @@
+package net.jayde.study.Algorithms4.c1_3_BagQueryStack;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class E1_3_1 {
+
+    private static Logger logger = LogManager.getLogger(E1_3_1.class.getName());
+
+}
